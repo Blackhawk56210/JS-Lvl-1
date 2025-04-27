cd62799889bd7921b989dd6b0c417d5fff2a799f
Colby <maquiret@gmail.com>
2025-04-26 23:18:58 -0500

    fin

--- Jocelin-Hw/to-do-list/script.js
+++ Jocelin-Hw/to-do-list/script.js
@@ -1,1 +1,27 @@
-console.log("I created the perfect system");
+function addTask() {
+    let taskInput = document.getElementById("task");
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(taskInput.value);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTasks();
+    taskInput.value = "";
+}
+
+function displayTasks() {
+    let taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+    let tasks = JSON.parse(localStorage.getITEM("tasks")) || [];
+    tasks,forEach((tasks, index) => {
+        const li = document.createElement(li);
+        li.innerHTM = `${task} <button onclick="removeTask(${index})">x</button>`;
+        taskList.appendChild(li);
+    });
+};
+
+function removeTask(index) {
+    let tasks = JSON.parse(localStorage.getITEM("tasks")) || [];
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTasks();
+    displayTasks();
+}

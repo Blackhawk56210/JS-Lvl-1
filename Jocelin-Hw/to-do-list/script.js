5e994857cd56eb53a86d86b09a53bfa0c7de98a5
Colby <maquiret@gmail.com>
2025-04-28 19:16:07 -0500

    changed some syntax

--- Jocelin-Hw/to-do-list/script.js
+++ Jocelin-Hw/to-do-list/script.js
@@ -1,27 +1,26 @@
 function addTask() {
-    let taskInput = document.getElementById("task");
-    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    tasks.push(taskInput.value);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    displayTasks();
+    let taskInput = document.getElementById("inputField");
+    let task = JSON.parse(localStorage.getItem("tasks")) || [];
+    task.push(taskInput.value);
+    localStorage.setItem("tasks", JSON.stringify(task));
+    displayTask();
     taskInput.value = "";
-}
-
-function displayTasks() {
+  }
+  
+  function displayTask() {
     let taskList = document.getElementById("taskList");
     taskList.innerHTML = "";
-    let tasks = JSON.parse(localStorage.getITEM("tasks")) || [];
-    tasks,forEach((tasks, index) => {
-        const li = document.createElement(li);
-        li.innerHTM = `${task} <button onclick="removeTask(${index})">x</button>`;
-        taskList.appendChild(li);
-    });
-};
-
-function removeTask(index) {
-    let tasks = JSON.parse(localStorage.getITEM("tasks")) || [];
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach((tasks, index) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${tasks} <button onclick="removeTask(${index})">remove</button>`
+      taskList.appendChild(li);
+    })
+  }
+  
+  function removeTask(index) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     tasks.splice(index, 1);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    displayTasks();
-    displayTasks();
-}
+    displayTask();
+  }

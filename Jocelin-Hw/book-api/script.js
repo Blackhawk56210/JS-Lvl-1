4599484a5ec611d8ca03b8052bfae070b2d896b4
Colby <maquiret@gmail.com>
2025-05-02 00:21:38 -0500

    just added some API links

--- Jocelin-Hw/book-api/script.js
+++ Jocelin-Hw/book-api/script.js
@@ -1,1 +1,6 @@
 console.log("I created the perfect system");
+function fetchBooks() {
+    axios.get("https://openlibrary.org/search.json?q=key,title,authorname,editions")
+    // Use to fetch covers for books and authors => https://covers.openlibrary.org/a/olid/OL23919A-M.jpg
+    // ??
+}
